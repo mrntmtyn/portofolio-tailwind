f1f3d5fda72721724f3612f9015cb832f121418c
mrntmtyn <mirantimitayani0@gmail.com>
2024-04-18 16:31:12 +0700

    init project portofolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#1e293b',
+      },
+      screens: {
+        '2xl': '1320px'
+      },
+    },
+  },
+  plugins: [],
+}
+
